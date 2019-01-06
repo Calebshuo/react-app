9e118fd7bd28211a94d203fdc34cd8caaf40c3b2
li.shuo <444947389@qq.com>
2019-01-06 16:08:08 +0800

    npm run eject

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd-mobile';
 
-class App extends Component {
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Hello></Hello>
+        <h2>World</h2>
+        <Talk name='abc'></Talk>
       </div>
-    );
+    )
   }
 }
+function Talk(props) {
+  return (<div>
+    <h1>talk,{props.name}</h1>
+    <Button type="primary">push</Button>
+  </div>)
+         
+}
 
+class Hello extends React.Component {
+  render() {
+    let name = 'ls'
+    return <h2> hello{name}</h2>
+  }
+}
 export default App;
